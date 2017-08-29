dd1d3e7afb1304f1a0c14f04580148790b2cf30a
woodylewis <woody@woodylewis.com>
2017-08-28 20:01:29 -0400

    add basic router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
+const routes: Routes = [
+    { path: 'home', component: AppComponent }
+]; 
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
